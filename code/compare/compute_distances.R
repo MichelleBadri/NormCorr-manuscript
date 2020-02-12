ee39f1329dd62e2fa8b4b4d989f93fa428d8f27e
MichelleBadri <MichelleBadri@users.noreply.github.com>
2020-02-11 20:55:05 -0500

    Add pearson correlation to distance plots

--- code/compare/compute_distances.R
+++ code/compare/compute_distances.R
@@ -2,20 +2,34 @@ source('code/compare/metrics.R')
 
 ## Load in correlation estimates
 est.list <- readRDS("data/RDS/est_all.RDS")
+
+## Calculate pearson correlation from amgut estimates
+list.raw <- unlist(lapply(est.list, function(x) x[grep("amgut", names(x))]), recursive=FALSE)
+cor.list <- lapply(list.raw,cor)
+
+names(cor.list)<- paste(sapply(strsplit(names(cor.list),split="\\."), `[`, 1), sapply(strsplit(names(cor.list),split="\\."), `[`, 3),"cor",sep=".")
+
 ## Keep correlations/proportionalities and unlist 1 level
 est.list <- unlist(lapply(est.list, function(x) x[grep("cor|rho", names(x))]), recursive=FALSE)
 
+## Combine with corshrink and proportionality
+est.list.cor <- c(est.list,cor.list)
+
+saveRDS(est.list.cor, file="data/RDS/est.list.corr.RDS")
+
 ## Pairwise correlation matrix distances via parallelDist package
-forst.all <- forstner.dist(est.list)
-riemn.all <- riemannian.dist(est.list)
-frob.all  <- frobenius.dist(est.list)
+#forst.all.cor <- forstner.dist(est.list.cor)
+#riemn.all.cor <- riemannian.dist(est.list.cor)
+frob.all.cor  <- frobenius.dist(est.list.cor)
 spect.all <- spectral.dist(est.list)
-cmd.all   <- corrmat.dist(est.list)
+cmd.all   <- corrmat.dist(est.list.cor)
+
 
 ## Save all as an RDS object
-dmat_all <- list(forst=forst.all, riemn=riemn.all,
-                 frob=frob.all,
-                 spect=spect.all, cmd=cmd.all)
-attr(dmat_all, 'datasets') <- names(est.list)
+dmat_all_cor <- list(#forst=forst.all_cor, riemn=riemn.all_cor,
+                     frob=frob.all.cor,
+                     #spect=spect.all, 
+                     cmd=cmd.all)
+attr(dmat_all_cor, 'datasets') <- names(est.list.cor)
 
-saveRDS(dmat_all, file="data/RDS/dmat_all.RDS")
+saveRDS(dmat_all_cor, file="data/RDS/dmat_all_cor.RDS")
--- code/compare/data_helpers.R
+++ code/compare/data_helpers.R
@@ -3,20 +3,30 @@ suppressPackageStartupMessages(library(dplyr))
 
 ## Read in distance matrices, method colors, metadata, etc...
 
-dmat_all <- readRDS(file="data/RDS/dmat_all.RDS")
+dmat_all <- readRDS(file="data/RDS/dmat_all_cor.RDS")
 
 ## method color named vector
 col   <- unlist(yaml::yaml.load_file('code/helpers/colors.yml'))
+
+## method shape named vector
+shape   <- unlist(yaml::yaml.load_file('code/helpers/pointshape.yml'))
 
 ## Dataset types
 datasets <- attr(dmat_all, 'datasets')
 dtypes <- stringr::str_split_fixed(datasets, "_", 2)[,2]
 coldf <- data.frame(method=names(col), col=col, stringsAsFactors=FALSE)
+shapedf <- data.frame(method=names(shape), shape=shape, stringsAsFactors=FALSE)
 ddf <- stringr::str_split_fixed(datasets, "_|\\.", 4) %>%
           as.data.frame(stringsAsFactors=FALSE) %>%
           transmute(dtype=dtypes,
                  rep=as.numeric(V1),
                    n=as.numeric(V2),
-                 method=V3) %>%
+                 method=V3,
+                 association=V4,
+                 group=paste(V3,V4,sep="")) %>%
           left_join(coldf, by = "method")
+ddf<- left_join(shapedf,ddf, by = "method")
+ddf[ddf$method=="rhoprop",]$association <- "propr"
+ddf[ddf$method=="rhoshrink",]$association <- "corshrink"
 rm(coldf)
+rm(shapedf)
